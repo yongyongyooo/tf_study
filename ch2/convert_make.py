e20b0d44a6a48e58229c97b7949223f2babac269
yongyongyuuuuuu <32666693+yongyongyooo@users.noreply.github.com>
2017-10-19 17:29:40 +0900

    Update and rename color_convert.py to convert_make.py

--- ch2/convert_make.py
+++ ch2/convert_make.py
@@ -4,23 +4,26 @@ import sys
 import os
 
 train_path = './train/'
+
 convert_path = './convert/'
 test_path = './test/'
 
 set_size = 3000
+test_size = 1000
+
 if len(sys.argv) == 2:
     set_size = int(sys.argv[1])
 
-c_size = 256
+c_size = 128
 origin_data = os.listdir('./train')
 random.shuffle(origin_data)
 
 print('train set make')
 for idx, d in enumerate(origin_data[:set_size]):
-    Image.open(train_path + d).convert('1').resize((c_size, c_size)).save(convert_path + d)
+    Image.open(train_path + d).resize((c_size, c_size)).save(convert_path + d)
     print('{} idx image finish {} remain'.format(idx, set_size - idx))
 
 print('test set make')
-for idx, d in enumerate(origin_data[-100:]):
-    Image.open(train_path + d).convert('1').resize((c_size, c_size)).save(test_path + d)
-    print('{} idx image finish {} remain'.format(idx, 100 - idx))
+for idx, d in enumerate(origin_data[-test_size:]):
+    Image.open(train_path + d).resize((c_size, c_size)).save(test_path + d)
+    print('{} idx image finish {} remain'.format(idx, test_size - idx))
