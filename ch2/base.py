f9e3fd213de51534f98bfcd0f4ae2493408319fa
yongyongyuuuuuu <32666693+yongyongyooo@users.noreply.github.com>
2017-10-19 17:29:09 +0900

    Update base.py

--- ch2/base.py
+++ ch2/base.py
@@ -2,15 +2,17 @@ import tensorflow as tf
 
 import numpy as np
 import utils
-from utils import get_batch, get_test, c_size, data_size
+import random
+from PIL import Image
+from utils import get_batch, get_test, c_size, data_size, test_dir, test_path
 
-batch_size = 100
+batch_size = 10
 hidden_size = 512
 
-X = tf.placeholder(tf.float32, [None, c_size * c_size], name='x-input')
+X = tf.placeholder(tf.float32, [None, c_size * c_size * 3], name='x-input')
 Y = tf.placeholder(tf.float32, [None, 2], name='y-input')
 
-W1 = tf.Variable(tf.random_normal([c_size*c_size, hidden_size]))
+W1 = tf.Variable(tf.random_normal([c_size*c_size*3, hidden_size]))
 b1 = tf.Variable(tf.random_normal([hidden_size]))
 layer1 = tf.sigmoid(tf.matmul(X, W1) + b1)
 
@@ -24,7 +26,7 @@ optimizer = tf.train.AdamOptimizer(learning_rate=1e-5).minimize(cost)
 sess = tf.Session()
 sess.run(tf.global_variables_initializer())
 
-for epoch in range(20):
+for epoch in range(10):
     total_cost = 0
     goal_page = data_size // batch_size
 
@@ -32,7 +34,9 @@ for epoch in range(20):
         batch_x, batch_y = get_batch(page, batch_size)
         c, _ = sess.run([cost, optimizer], feed_dict={X:batch_x, Y: batch_y})
         total_cost += c
-        print(page, '/', goal_page)
+
+        if page % 10 == 0:
+            print(page, '/', goal_page)
 
     print('epoch :', epoch, 'cost : ', total_cost / goal_page)
 
@@ -43,3 +47,17 @@ accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 test_data, test_label = get_test()
 
 print('Accuracy:', sess.run(accuracy, feed_dict={ X: test_data, Y: test_label}))
+
+while True:
+    r = random.randint(0, len(test_dir) - 1)
+    test_x = test_dir[r]
+
+    print(test_x)
+    test_img = Image.open(test_path + test_x)
+    # print("Label: ", sess.run(tf.argmax(mnist.test.labels[r:r + 1], 1)))
+    # print("Prediction: ", sess.run(
+    #     tf.argmax(hypothesis, 1), feed_dict={X: mnist.test.images[r:r + 1]}))
+    print(sess.run(hypothesis, feed_dict={X: test_img}))
+
+    plt.imshow(test_img, cmap='Greys', interpolation='nearest')
+    plt.show()
