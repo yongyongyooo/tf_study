29901d29bba9d7e07578d6b7645a3ea0747e0574
yongyongyuuuuuu <32666693+yongyongyooo@users.noreply.github.com>
2017-10-31 19:27:33 +0900

    Update utils.py

--- ch2/utils.py
+++ ch2/utils.py
@@ -6,20 +6,21 @@ from PIL import Image
 train_path = './convert/'
 test_path = './test/'
 
-data_size = 3000
+data_size = 1000
 c_size = 128
 imgs = list(filter(lambda x: x != 'Thumbs.db', os.listdir(train_path)))
 random.shuffle(imgs)
 imgs = imgs[:data_size]
 test_dir = list(filter(lambda x: x != 'Thumbs.db', os.listdir(test_path)))
 
+
 def get_test():
-    datas = []    
+    datas = []
     for d in test_dir:
         im = Image.open(test_path + d)
         datas.append(np.array(im))
 
-    datas = np.array(datas).reshape((data_size, c_size * c_size * 3))
+    datas = np.array(datas)
 
     labels = []
 
@@ -33,27 +34,28 @@ def get_test():
         labels.append(tmp)
 
     labels = np.array(labels)
-    
+
     return (datas, labels)
 
 
 def get_img(page, size):
     t = []
 
-    for d in imgs[page*size:(page+1)*size]:
+    for d in imgs[page * size:(page + 1) * size]:
         im = Image.open(train_path + d)
         t.append(np.array(im))
 
-    t = np.array(t).reshape((size, c_size * c_size * 3))
+    t = np.array(t)
 
     return t
+
 
 # cat : 0
 # dog : 1
 def get_label(page, size):
     t = []
 
-    for d in imgs[page*size:(page+1)*size]:
+    for d in imgs[page * size:(page + 1) * size]:
         tmp = [0, 0]
         if d.split('.')[0] == 'cat':
             tmp[0] = 1
@@ -64,6 +66,7 @@ def get_label(page, size):
 
     t = np.array(t)
     return t
+
 
 def get_batch(page, size):
     return (get_img(page, size), get_label(page, size))
