05a29d138dff7d9184b103a10f53be33f8362454
yongyongyuuuuuu <32666693+yongyongyooo@users.noreply.github.com>
2017-11-01 12:39:57 +0900

    Update base.py

--- ch2/base.py
+++ ch2/base.py
@@ -1,72 +1,68 @@
-import random
-import os
+import tensorflow as tf
+
 import numpy as np
+import random
 from PIL import Image
+from dutils import get_batch, get_test, c_size, data_size, test_dir, test_path
+import matplotlib.pyplot as plt
 
-train_path = './convert/'
-test_path = './test/'
+batch_size = 10
+hidden_size = 512
 
-data_size = 3000
-c_size = 128
-imgs = list(filter(lambda x: x != 'Thumbs.db', os.listdir(train_path)))
-random.shuffle(imgs)
-imgs = imgs[:data_size]
-test_dir = list(filter(lambda x: x != 'Thumbs.db', os.listdir(test_path)))
+X = tf.placeholder(tf.float32, [None, c_size * c_size * 3], name='x-input')
+Y = tf.placeholder(tf.float32, [None, 2], name='y-input')
 
+W1 = tf.Variable(tf.random_normal([c_size * c_size * 3, hidden_size]))
+b1 = tf.Variable(tf.random_normal([hidden_size]))
+layer1 = tf.sigmoid(tf.matmul(X, W1) + b1)
 
-def get_test():
-    datas = []
-    for d in test_dir:
-        im = Image.open(test_path + d)
-        datas.append(np.array(im))
+W2 = tf.Variable(tf.random_normal([hidden_size, 2]))
+b2 = tf.Variable(tf.random_normal([2]))
+hypothesis = tf.sigmoid(tf.matmul(layer1, W2) + b2)
 
-    datas = np.array(datas).reshape(1000, c_size * c_size * 3)
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=hypothesis, labels=Y))
+optimizer = tf.train.AdamOptimizer(learning_rate=1e-5).minimize(cost)
 
-    labels = []
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
 
-    for d in test_dir:
-        tmp = [0, 0]
-        if d.split('.')[0] == 'cat':
-            tmp[0] = 1
-        else:
-            tmp[1] = 1
+for epoch in range(5):
+    total_cost = 0
+    goal_page = data_size // batch_size
 
-        labels.append(tmp)
+    for page in range(goal_page):
+        batch_x, batch_y = get_batch(page, batch_size)
+        c, _ = sess.run([cost, optimizer], feed_dict={X: batch_x, Y: batch_y})
+        total_cost += c
 
-    labels = np.array(labels)
+        if page % 10 == 0:
+            print(page, '/', goal_page)
 
-    return (datas, labels)
+    print('epoch :', epoch, 'cost : ', total_cost / goal_page)
 
+correct_prediction = tf.equal(tf.argmax(hypothesis, 1), tf.argmax(Y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
-def get_img(page, size):
-    t = []
+test_data, test_label = get_test()
 
-    for d in imgs[page * size:(page + 1) * size]:
-        im = Image.open(train_path + d)
-        t.append(np.array(im))
+print('Accuracy:', sess.run(accuracy, feed_dict={X: test_data, Y: test_label}))
 
-    t = np.array(t).reshape((size, c_size * c_size * 3))
+while True:
+    r = random.randint(0, len(test_dir) - 1)
+    test_x = test_dir[r]
 
-    return t
+    print('label : ', test_x.split('.')[0])
+    test_img = Image.open(test_path + test_x)
+    temp_img = np.array(test_img).reshape((1, c_size * c_size * 3))
+    # print("Label: ", sess.run(tf.argmax(mnist.test.labels[r:r + 1], 1)))
+    # print("Prediction: ", sess.run(
+    #     tf.argmax(hypothesis, 1), feed_dict={X: mnist.test.images[r:r + 1]}))
+    result = sess.run(hypothesis, feed_dict={X: temp_img})
 
+    if result[0][0] > result[0][1]:
+        print('예측 : cat')
+    else:
+        print('예측 : dog')
 
-# cat : 0
-# dog : 1
-def get_label(page, size):
-    t = []
-
-    for d in imgs[page * size:(page + 1) * size]:
-        tmp = [0, 0]
-        if d.split('.')[0] == 'cat':
-            tmp[0] = 1
-        else:
-            tmp[1] = 1
-
-        t.append(tmp)
-
-    t = np.array(t)
-    return t
-
-
-def get_batch(page, size):
-    return (get_img(page, size), get_label(page, size))
+    plt.imshow(test_img, cmap='Greys', interpolation='nearest')
+    plt.show()
